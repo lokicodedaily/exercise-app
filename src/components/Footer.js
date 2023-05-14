a262dee293435f1b1646e0ba418d43d46839f058
lokicodedaily <you@example.com>
2023-05-15 01:17:36 +0530

    full App

---
+++ src/components/Footer.js
@@ -0,0 +1,20 @@
+import React from "react";
+import { Box } from "@mui/material";
+
+
+const Footer = () => (
+  <Box
+    mt="80px"
+    bgcolor="#ed959c"
+    style={{
+      textAlign: "center",
+      fontSize: "20px",
+      padding: "10px 0",
+      color: "#fff",
+    }}
+  >
+    Muscle Mentor
+  </Box>
+);
+
+export default Footer;
